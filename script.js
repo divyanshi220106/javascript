1ba7cf7fc8d123641e6f875325558817156f32a6
divyanshi <divyanshiagarwal22@gmail.com>
2024-12-19 16:21:33 +0530

    java

---
+++ script.js
@@ -0,0 +1,80 @@
+// var grade='A';
+// document.write("Entering switch block:");
+// switch(grade){
+//     case'A':document.write("Good job");
+//     break;
+//     case'B':document.write("Good job");
+//     break;
+//     case'C':document.write("pretty good");
+//     break;
+//     case'D':document.write("not so good")
+//     break;
+// }
+// 
+// var myArray=["A","B","C"];
+// for(var i=0;i<myArray.length;i++){
+//    console.log("the memeber of myArray in index"+i+"is"+myArray[i]);
+// }
+// function myFunction(){
+//     alert("Hello world!")
+// }
+// function sayHello(name,age){
+//     document.write(name+"is"+age+"years old");
+// }
+// function add(a,b){
+//     console.log(a+b);
+//     document.write(a+b);
+// }
+// add(10,20);
+// var res=mul(12,30);
+// function mul(x,y){
+//     return x*y;
+// }
+// document.write(res);
+// const square=function(number){
+// //     return number*number;
+// const x=square(4);
+// console.log(x);
+// function myFunction(g1,g2){
+//     return g1/g2;
+// }
+// const value=myFunction(8,2);
+// console.log(value);
+
+// function add(a,b){
+//       console.log(a+b);
+// }
+// add(10,20);
+// var add2=(a,b)=>{
+//     console.log(a+b);
+// };
+// add2(102,20);
+// var add3=(a,b)=>{
+//     console.log(a+b);
+// }
+// add3(30,20);
+
+// var a=99;
+// var b=13;
+// var c=15;
+// var linebreak="<br>";
+// document.write("a-b=");
+// result=a-b;
+// document.write(result);
+// document.write(linebreak);
+// document.write("a/b=");
+// result=a/b;
+// document.write(result);
+// document.write(linebreak);
+// document.write("a%b=");
+// result=a%b;
+// document.write(result);
+// document.write(linebreak);
+function myFunction(){
+var x=3+3;
+var y="3"+3;
+var z="Hello"+3;
+var demoP=document.getElementById("demo");
+demoP.innerHTML=x+"<br>"+y+"<br>"+z;
+}
+ 
