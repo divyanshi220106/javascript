31042305417531b0d0afd099cea09d746f6940a9
divyanshi220106 <163905472+divyanshi220106@users.noreply.github.com>
2025-01-15 15:26:59 +0530

    Add files via upload

---
+++ forloop.js
@@ -0,0 +1,38 @@
+// const person={fname:"John",lname: "Doe", age:25};
+// let text="";
+// for(let x in person){
+//     text+=person[x];
+// }
+// console.log(text);
+//   const numbers=[45,4,9,16,25];
+//   let txt="";
+//   for(let x in numbers){
+//     txt+=numbers[x];
+//   }
+//   console.log(txt);
+// const numbers=[45,4,9,16,25];
+// let txt="";
+// numbers.forEach(myFunction);
+// function myFunction(value,index,array){
+//     txt+=array;
+// }
+// document.write(txt);
+// const numbers=[45,4,9,16,25];
+// let txt="";
+// numbers.forEach(myFunction);
+// function myFunction(value){
+//     txt+=value;
+// }
+// document.write(txt);
+// const cars=["BMW","Volvo","Mini"];
+// let text="";
+// for(let x of cars){
+//     text+=x;
+// }
+// document.write(text);
+let language="JavaScript";
+let text="";
+for(let x of language){
+    text+=x;
+}
+document.write(text);
---
+++ regularexpression.js
@@ -0,0 +1,13 @@
+// let text="visit ABESEC!";
+// let n=text.search("ABESEC");
+// console.log(n);
+// let text="visit ABESEC!";
+// let n=text.replace("ABESEC","Microsoft");
+// console.log(n);
+
+// constpattern=/e/;
+// console.log(pattern.test("The best things in life are free!"));
+// const pattern= /e/;
+//         let x=pattern.exec("The best things in life are free")
+//         console.log(x);
+     
